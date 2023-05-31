21f1bb5c58f156e3fe4eb01d4f8f87738076d7ab
ntxLanb1906380 <101681858+ntxLanb1906380@users.noreply.github.com>
2023-05-31 09:46:06 +0700

    createNV.php

--- createNV.php
+++ createNV.php
@@ -1,33 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
- <link rel="stylesheet" href="style2.css"/> 
+ <link rel="stylesheet" href="css/style2.css"> 
 </head>
-<body class="a">
+<body class = "c">
 
 <form method="post" action="xulyDKy.php" class="form">
 
 <h2>Tạo tài khoản nhân viên mới</h2>
 
-Id: <input type="text" name="id" value="" required>
+Id: <input type="int" name="id" value="" required>
 
-Mã nhân viên: <input type="text" name="mnv" value="" required/>
+Mã nhân viên: <input type="varchar" name="mnv" value="" required/>
 
-Họ tên: <input type="text" name="hoten" value="" required/>
+Họ tên: <input type="nvarchar" name="hoten" value="" required/>
 
-Ngày sinh: <input type="text" name="ngsinh" value="" required>
+Ngày sinh: <input type="date" name="ngsinh" value="" required>
 
-Số điện thoại: <input type="text" name="sdt" value="" required/>
+Số điện thoại: <input type="char" name="sdt" value="" required/>
 
-Địa chỉ: <input type="text" name="diachi" value="" required>
+Địa chỉ: <input type="nvarchar" name="diachi" value="" required>
 
-Chức vụ: <input type="text" name="chucvu" value="" required>
+Chức vụ: <input type="nvarchar" name="chucvu" value="" required>
 
-Email: <input type="text" name="email" value="" required>
+Email: <input type="varchar" name="email" value="" required>
 
-Giới tính: <input type="text" name="gtinh" value="" required>
+Giới tính: <input type="nvarchar" name="gtinh" value="" required>
 
-Mã phòng ban: <input type="text" name="mapb" value="" required>
+Mã phòng ban: <input type="int" name="mapb" value="" required>
 
 Mật khẩu: <input type="password" name="pw" value="" required>
 
