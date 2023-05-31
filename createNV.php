173ec3c37302826c9925a652968ed0f85e0d846b
ntxLanb1906380 <101681858+ntxLanb1906380@users.noreply.github.com>
2023-05-31 16:49:03 +0700

    createNV.php

--- createNV.php
+++ createNV.php
@@ -20,36 +20,22 @@ $conn = sqlsrv_connect($serverName, array(
 
 $max_id_query = "SELECT MAX(id) AS max_id FROM nhanvien;";
 $result = sqlsrv_query($conn, $max_id_query);
-$row = sqlsrv_fetch_array($result);
+$row = sqlsrv_fetch_array($result,  SQLSRV_FETCH_ASSOC);
 $new_id = $row['max_id'] + 1;
+$new_mnv = strval($row['max_id'] + 1);
+if (strlen($new_mnv) == 1) {
+    $new_mnv = "0" . $new_mnv;
+}
+
+$mnv = "NV" . $new_mnv;
 ?>
 
-<!-- <form method="post" action="xulyDKy.php" class="form">
-
 <h2>Tạo tài khoản nhân viên mới</h2>
-//html không hỗ trọ int nên thay thế bằng number, không hỗ trợ varchar, char nên thay thế bằng text
-Id: <input type="int" name="id" value="<?php echo $new_id; ?>" required>
-Mã nhân viên: <input type="varchar" name="mnv" value="" required/>
-Họ tên: <input type="nvarchar" name="hoten" value="" required/>
-Ngày sinh: <input type="date" name="ngsinh" value="" required>
-Số điện thoại: <input type="char" name="sdt" value="" required/>
-Địa chỉ: <input type="nvarchar" name="diachi" value="" required>
-Chức vụ: <input type="nvarchar" name="chucvu" value="" required>
-Email: <input type="varchar" name="email" value="" required>
-Giới tính: <input type="nvarchar" name="gtinh" value="" required>
-Mã phòng ban: <input type="int" name="mapb" value="" required>
-Mật khẩu: <input type="password" name="pw" value="" required>
-Nhập lại mật khấu: <input type="password" name="pw" value="" required>
-<input type="submit" name="createNV" value="Tạo tài khoản"/>
-<?php require 'xulyDKy.php';?>
-</form> -->
-
+<!-- html không hỗ trọ int nên thay thế bằng number, không hỗ trợ varchar, char nên thay thế bằng text -->
 <form method="post" action="xulyDKy.php" class="form">
 
-  <h2>Tạo tài khoản nhân viên mới</h2>
-
-  Id: <input type="number" name="id" value="<?php echo $new_id; ?>" required>
-  Mã nhân viên: <input type="text" name="mnv" value="" required />
+  Id: <input type="text" name="id" value="<?php echo $new_id; ?>" required>
+  Mã nhân viên: <input type="text" name="mnv" value="<?php echo $mnv; ?>" required />
   Họ tên: <input type="text" name="hoten" value="" required />
   Ngày sinh: <input type="date" name="ngsinh" value="" required>
   Số điện thoại: <input type="tel" name="sdt" value="" required />
