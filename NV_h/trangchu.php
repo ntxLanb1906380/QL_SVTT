35b9edc46b942f1c6e73288adcbcb1efb3aee502
ntxLanb1906380 <101681858+ntxLanb1906380@users.noreply.github.com>
2023-06-30 10:02:35 +0700

    Update trangchu.php

--- NV_h/trangchu.php
+++ NV_h/trangchu.php
@@ -9,31 +9,31 @@
     </head>
     <h2>Trang chủ </h2>
     <?php
-    session_start();
     include "connect.php";
-    $idnv = $_GET["id"];
-
-    //lưu biến idnv vào session
-    $_SESSION["idnv"] = $idnv;
     ?>
     <ul>
         <li>
-            <a href="cocautc.php?id=<?php echo $idnv; ?>">Xem cơ cấu tổ chức</a>
+            <a href="cocautc.php">Xem cơ cấu tổ chức</a>
         </li>
         </br>
 
         <li>
-            <a href="chonDSSVkhoa.php?id=<?php echo $idnv; ?>">Xem danh sách sinh viên hướng dẫn</a>
+            <a href="chonDSSVkhoa.php">Xem danh sách sinh viên hướng dẫn</a>
         </li>
         </br>
 
         <li>
-            <a href="thtinNV.php?id=<?php echo $idnv; ?>">Xem thông tin cá nhân</a>
+            <a href="thtinNV.php">Xem thông tin cá nhân</a>
         </li>
         </br>
 
         <li>
-            <a href="doiMK.php?id=<?php echo $idnv; ?>">Đổi mật khẩu</a>
+            <a href="doiMK.php">Đổi mật khẩu</a>
+        </li>
+        </br>
+
+        <li>
+            <a href="logout.php">Đăng xuất</a>
         </li>
 </body>
 
