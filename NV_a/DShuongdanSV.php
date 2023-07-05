d4744645e4ac6c383abff39c1053450dcd211ba9
ntxLanb1906380 <101681858+ntxLanb1906380@users.noreply.github.com>
2023-07-05 08:12:18 +0700

    Update DShuongdanSV.php

--- NV_a/DShuongdanSV.php
+++ NV_a/DShuongdanSV.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <style>
-    .e {
+    table {
         text-align: center;
     }
 </style>
@@ -10,14 +10,23 @@
     <link rel="stylesheet" href="css/style2.css">
     <meta charset="UTF-8">
     <title>
-        Danh sách sinh viên một khóa
+        Cập nhật ID hướng dẫn
     </title>
 </head>
 
 <body>
     <?php
     include "connect.php";
-    $mkhoa = $_GET["makhoa"];
+    session_start();
+
+    if (isset($_SESSION['makhoa'])) {
+        $mkhoa = $_SESSION['makhoa'];
+    } else {
+        $mkhoa = $_GET["makhoa"];
+
+        //lưu biến $mkhoa vào session
+        $_SESSION["makhoa"] = $mkhoa;
+    }
     $sql = "SELECT * FROM sinhvien WHERE makhoa = ?";
     $param = array($mkhoa);
     $stmt = sqlsrv_query($conn, $sql, $param);
@@ -30,104 +39,88 @@
         <h2 align="center">Danh sách sinh viên khóa
             <?php echo $mkhoa ?>
         </h2>
-        <table border="2" align="center">
-            <tr>
-                <td>Mssv</td>
-                <td>Họ tên</td>
-                <td>Mã trường</td>
-                <td>Mã khóa</td>
-                <td>Nội dung thực tập</td>
-                <td>Kết quả thực tập</td>
-                <td>Id cán bộ hướng dẫn</td>
-                <td>Sửa</td>
-            </tr>
-
-            <?php
-            while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-                $currentID = $row['id'];
-                ?>
+        <!-- <form action="luuThayDoiSV.php" method="POST" onsubmit="return luuThayDoi()"> -->
+        <form action="luuThayDoiSV.php" method="POST">
+            <table border="2">
                 <tr>
-                    <td>
-                        <?php echo $row["mssv"]; ?>
-                    </td>
-                    <td>
-                        <?php echo $row["hoten"]; ?>
-                    </td>
-                    <td>
-                        <?php echo $row["matruong"]; ?>
-                    </td>
-                    <td>
-                        <?php echo $row["makhoa"]; ?>
-                    </td>
-                    <td>
-                        <?php echo $row["noidungTT"]; ?><br />
-                        <input type="text" id="input-noidungTT-<?php echo $row["noidungTT"]; ?>"
-                            placeholder="Nhập nội dung thực tập mới">
-
-                    </td>
-                    <td>
-                        <?php echo $row["ketqua"]; ?><br />
-                        <input type="text" id="input-ketqua-<?php echo $row["ketqua"]; ?>"
-                            placeholder="Nhập kết quả thực tập mới">
-
-                    </td>
-                    <!-- lưu trực tiếp $ndtt và $kqtt tại trang này -->
-                    <td>
-                        <select id="idcb" name="idcb" class="e">
-                            <?php
-                            //Tạo câu truy vấn để lấy id cán bộ hướng dẫn
-                            $sql3 = "SELECT * FROM nhanvien";
-                            $stmt3 = sqlsrv_query($conn, $sql3);
-                            if ($stmt3 === false) {
-                                die(print_r(sqlsrv_errors(), true));
-                            }
-                            while ($row3 = sqlsrv_fetch_array($stmt3, SQLSRV_FETCH_ASSOC)) {
-                                $selected3 = ($row3['id'] == $currentID) ? 'selected="selected"' : '';
-                                echo "<option value='" . $row3['id'] . "' " . $selected3 . ">" . $row3['id'] . " - " . $row3['manv'] . " - " . $row3['hoten'] . "</option>";
-                            }
-                            sqlsrv_free_stmt($stmt3);
-                            ?>
-                        </select> <br /><br />
-                    </td>
-                    <td>
-                        <button type="button" onclick="luuThayDoi('<?php echo $row['mssv']; ?>')">Lưu thay đổi</button>
-                        <!-- <a href="editSV.php?mssv=<?php echo $row['mssv']; ?> ">Lưu thay đổi</a> -->
-                    </td>
+                    <td>Mssv</td>
+                    <td>Họ tên</td>
+                    <td>Mã trường</td>
+                    <td>Mã khóa</td>
+                    <td>Id nhân viên</td>
+                    <td>Lưu thay đổi</td>
                 </tr>
+
                 <?php
+                while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+                    $currentID = $row['id'];
+                    ?>
+                    <tr id="row-<?php echo $row['mssv']; ?>">
+                        <td>
+
+                            <input type="hidden" name="mssv" value="<?php echo $row["mssv"]; ?>">
+                            <?php
+                            echo $row["mssv"];
+                            ?>
+                        </td>
+                        <td>
+                            <?php echo $row["hoten"]; ?>
+                        </td>
+                        <td>
+                            <?php echo $row["matruong"]; ?>
+                        </td>
+                        <td>
+                            <?php echo $row["makhoa"]; ?>
+                        </td>
+                        <td>
+                            <input type="hidden" name="id" value="<?php echo $row["id"]; ?>">
+                            <input class="limit-width" type="text" name="idnv" value="<?php echo $row["id"]; ?>" oninput="luuThayDoi('<?php echo $row['mssv']; ?>', this.value)">
+                        </td>
+                        <td>
+                            <button type="submit">Lưu</button>
+                        </td>
+                    </tr>
+                    <?php
+                }
+                ?>
+            </table>
+        </form>
+
+        <h3>Danh sách nhân viên</h3>
+        <select id="idcb" name="idcb">
+            <?php
+            //Tạo câu truy vấn để lấy id cán bộ hướng dẫn
+            $sql3 = "SELECT * FROM nhanvien";
+            $stmt3 = sqlsrv_query($conn, $sql3);
+            if ($stmt3 === false) {
+                die(print_r(sqlsrv_errors(), true));
             }
+            while ($row3 = sqlsrv_fetch_array($stmt3, SQLSRV_FETCH_ASSOC)) {
+                $selected3 = ($row3['id'] == $currentID) ? 'selected="selected"' : '';
+                echo "<option value='" . $row3['id'] . "' " . $selected3 . ">" . $row3['id'] . " - " . $row3['manv'] . " - " . $row3['hoten'] . "</option>";
+            }
+            sqlsrv_free_stmt($stmt3);
             ?>
-        </table>
+        </select> <br /><br />
         <?php
     }
     ?>
+
 </body>
 
 </html>
 <script>
-    function luuThayDoi(mssv) {
-  // Lấy giá trị mới của noidungTT và ketqua từ các input, ở đây ta giả sử các input có id lần lượt là input-noidungTT và input-ketqua
-        var inputNoidungTT = document.getElementById("input-noidungTT-" + mssv).value;
-        var inputKetqua = document.getElementById("input-ketqua-" + mssv).value;
+    function luuThayDoi(mssv, idnv) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+            }
+        };
 
-        // Tạo mới đối tượng XMLHttpRequest
-        var xhttp = new XMLHttpRequest();
+        var url = "luuThayDoiSV.php?mssv=" + mssv + "&id=" + idnv;
+        xhr.open("GET", url, true);
+        xhr.send();
 
-        // Gửi yêu cầu đến server thông qua phương thức POST và gửi dữ liệu lên server dưới dạng form data
-        xhttp.open("POST", "luuThayDoi.php", true);
-        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhttp.send("mssv=" + mssv + "&noidungTT=" + inputNoidungTT + "&ketqua=" + inputKetqua);
-
-        // Xử lý phản hồi từ server, thông báo lưu thành công hoặc hiển thị thông báo lỗi
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                if (this.responseText == "success") {
-                    alert("Lưu thành công!");
-                } else {
-            alert("Lưu thất bại!");
-      }
+        return false; // Ngăn chặn mặc định của sự kiện
     }
-  };
-}
-
 </script>
