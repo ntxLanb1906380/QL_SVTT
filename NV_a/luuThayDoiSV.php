35a2c6a244f09c559ce69dd3299217ee4c51faff
ntxLanb1906380 <101681858+ntxLanb1906380@users.noreply.github.com>
2023-07-05 08:12:49 +0700

    Update luuThayDoiSV.php

--- NV_a/luuThayDoiSV.php
+++ NV_a/luuThayDoiSV.php
@@ -1,21 +1,25 @@
+
 <?php
 include "connect.php";
 
-// Lấy giá trị được gửi lên từ client
-$mssv = $_POST["mssv"];
-$noidungTT = $_POST["noidungTT"];
-$ketqua = $_POST["ketqua"];
+// Lấy giá trị từ URL (GET)
+$mssv = $_GET['mssv']; 
+$id = $_GET['id']; 
 
-// Lưu giá trị vào database
-$sql = "UPDATE sinhvien SET noidungTT = ?, ketqua = ? WHERE mssv = ?";
-$params = array($noidungTT, $ketqua, $mssv);
+// Cập nhật giá trị vào database
+$sql = "UPDATE sinhvien SET id = ? WHERE mssv = ?";
+$params = array($id, $mssv);
 $stmt = sqlsrv_query($conn, $sql, $params);
 
-// Kiểm tra kết quả
+// Kiểm tra kết quả truy vấn
 if ($stmt === false) {
     die(print_r(sqlsrv_errors(), true));
 } else {
-    echo "success";
+    // Đóng kết nối database
+    sqlsrv_close($conn);
+
+    // Chuyển hướng người dùng đến trang DShuongdanSV.php
+    header("Location: DShuongdanSV.php");
+    exit();
 }
-sqlsrv_free_stmt($stmt);
 ?>
