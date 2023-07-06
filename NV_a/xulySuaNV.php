0b77c57a03d29b225f01801d92e2e093754e6a02
ntxLanb1906380 <101681858+ntxLanb1906380@users.noreply.github.com>
2023-07-06 11:05:41 +0700

    Update xulySuaNV.php

--- NV_a/xulySuaNV.php
+++ NV_a/xulySuaNV.php
@@ -1,10 +1,10 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 /* Database connection start */
-include "connect.php"; 
+include "connect.php";
 
 session_start();
-$id = $_SESSION["idnv"];
+$idcbhd = $_SESSION["idnvhd"];
 
 // Check if form is submitted
 if (isset($_POST['saveNV'])) {
@@ -32,42 +32,48 @@ if (isset($_POST['saveNV'])) {
 
 
     $maphongban = trim($_POST['mapb']);
-    $matkhau = trim($_POST['pw']);
+    $matkhau = trim($_POST['pwd']);
+    $passw = trim($_POST['pw']);
 
-
+    $errors = [];
     // Check if phone number or email already exist in database, add error if any
     $sql = "SELECT * FROM nhanvien WHERE (sdt = ? OR email = ?)";
     $params = array($sdt, $email);
     $stmt = sqlsrv_query($conn, $sql, $params);
     if ($stmt === false) { // Handle query error
+        // Sử dụng array_push để thêm thông báo lỗi vào mảng errors
         array_push($errors, "Database error: " . sqlsrv_errors()[0]['message']);
     } elseif (sqlsrv_has_rows($stmt)) { // Handle duplicate entry
-        // array_push($errors, "Số điện thoại hoặc email đã tồn tại trong hệ thống.");
-        echo "Số điện thoại hoặc email đã tồn tại trong hệ thống";
+        array_push($errors, "Số điện thoại hoặc email đã tồn tại trong hệ thống.");
         sqlsrv_free_stmt($stmt);
     } else {
         // No error, continue
         sqlsrv_free_stmt($stmt);
     }
-
+    
     //cập nhật thông tin nhân viên
-    $hashedPassword = password_hash($matkhau, PASSWORD_ARGON2ID);
-    $tsql = "UPDATE nhanvien SET hoten = ?, ngaysinh= ?, sdt= ?, diachi= ?, chucvu= ?, email= ?, gioitinh= ?, maphongban= ?, matkhau= ? WHERE id = ?";
-    $param = array($hoten, $ngaysinh, $sdt, $diachi, $chucvu, $email, $gioitinh, $maphongban, $hashedPassword, $id);
-    $result = sqlsrv_query($conn, $tsql, $param);
-    if ($result === false) {
-        $errors = sqlsrv_errors();
-        echo "<div class='alert alert-danger'>";
-        echo "<h4>Error:</h4>";
-        foreach ($errors as $error) {
-            echo "- " . $error['message'] . "<br>";
-            var_dump($result);
+    if ($matkhau !== null) {
+        $hashedPassword = password_hash($matkhau, PASSWORD_ARGON2ID);
+        $tsql = "UPDATE nhanvien SET hoten = ?, ngaysinh= ?, sdt= ?, diachi= ?, chucvu= ?, email= ?, gioitinh= ?, maphongban= ?, matkhau= ? WHERE id = ?";
+        $param = array($hoten, $ngaysinh, $sdt, $diachi, $chucvu, $email, $gioitinh, $maphongban, $hashedPassword, $idcbhd);
+        $result = sqlsrv_query($conn, $tsql, $param);
+        if ($result === false) {
+            $errors = sqlsrv_errors();
+            echo "<div class='alert alert-danger'>";
+            echo "<h4>Error:</h4>";
+            foreach ($errors as $error) {
+                echo "- " . $error['message'] . "<br>";
+                var_dump($result);
+            }
+            echo "</div>";
+        } else {
+            header('Location: DSNV.php');
         }
-        echo "</div>";
-    } else {
-        header('Location: DSNV.php');
+        sqlsrv_close($conn);
     }
-    sqlsrv_close($conn);
+    else {
+        $matkhau = $passw;
+    }
 }
 
 ?>
